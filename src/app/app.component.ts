768ec70b23714ac91feddbb7a406a34abbe73923
shubham sah <sah.shubham752@gmail.com>
2024-11-26 20:46:00 +0530

    using ngModel (1 way binding) to display data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'tdf';
 
+  userForm: any;
+
   topics = ['Angular', 'React', 'Vue'];
+
+  onSubmit(userForm: any) {
+    this.userForm = userForm;
+    console.log(this.userForm);
+
+    for (const fields in this.userForm) {
+      if (this.userForm[fields] == null || this.userForm[fields] == '') {
+        alert(`empty input field: ${fields}`);
+      }
+    }
+  }
 }
